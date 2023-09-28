5f2db893a0940ff12d9a4134d6d81f86b3213bc5
harshitverma2103 <harshitverma2103@gmail.com>
2023-09-28 22:54:04 +0530

    introduce a player in the game . when clicking on the square , show either X or O.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,15 +3,22 @@ import Square from './square';
 
 const Board = () => { 
   const [squares , setSquares] = useState(Array(9).fill(null));
+  const [isXNest , setIsXNext] = useState(false);
+
   const handleSquareclick = clickPosition => {
+    if(squares[clickPosition]) {
+      return;
+    }
     setSquares((currentSquare) => {
       return currentSquare.map((squaresValue , position) => {
         if (clickPosition === position) {
-          return 'X'; 
+          return isXNest ? 'X' : 'O'; 
         } 
         return squaresValue;     
       });
     });
+
+    setIsXNext((currentIsXNext) => !currentIsXNext);
   }
   
 
