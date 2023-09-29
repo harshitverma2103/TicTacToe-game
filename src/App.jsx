530ddfd073a3a9e291179d563d24f29a745cc864
harshitverma2103 <harshitverma2103@gmail.com>
2023-09-29 16:54:13 +0530

    lifted up the state to app component . show status message to the user , calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import './style.scss';
 import Board from './components/Board';
+import {calculateWinner} from './winner';
 
 function App() {
+  const [squares , setSquares] = useState(Array(9).fill(null));
+  const [isXNest , setIsXNext] = useState(false);
+
+  const winner =  calculateWinner(squares);
+  const nextPlayer = isXNest? 'X' : 'O';
+  const statusMessage = winner ? `Winner is ${winner}` : `next player is ${nextPlayer}`;
+  
+  console.log(winner);
+
+  const handleSquareclick = clickPosition => {
+    if(squares[clickPosition] || winner) {
+      return;
+    }
+    setSquares((currentSquare) => {
+      return currentSquare.map((squaresValue , position) => {
+        if (clickPosition === position) {
+          return isXNest ? 'X' : 'O'; 
+        } 
+        return squaresValue;     
+      });
+    });
+
+    setIsXNext((currentIsXNext) => !currentIsXNext);
+  }
   return (
     <div className="app">
-      <Board />
+      <h2> {statusMessage} </h2>
+      <Board squares={squares} handleSquareclick = {handleSquareclick}/>
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,7 @@
-import { useState } from 'react';
 import Square from './square';
 
-const Board = () => { 
-  const [squares , setSquares] = useState(Array(9).fill(null));
-  const [isXNest , setIsXNext] = useState(false);
-
-  const handleSquareclick = clickPosition => {
-    if(squares[clickPosition]) {
-      return;
-    }
-    setSquares((currentSquare) => {
-      return currentSquare.map((squaresValue , position) => {
-        if (clickPosition === position) {
-          return isXNest ? 'X' : 'O'; 
-        } 
-        return squaresValue;     
-      });
-    });
-
-    setIsXNext((currentIsXNext) => !currentIsXNext);
-  }
-  
-
+const Board = ({squares , handleSquareclick}) => { 
+ 
   const renderSquare = (position) => {
     return <Square value={squares[position]} onClick ={() => handleSquareclick(position)} />
   };
